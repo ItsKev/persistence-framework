82851bdcee176eb62f9c2f3cf086844ed257af96
Kevin Huber <kevin.huber@hotmail.com>
2021-08-22 13:32:18 +0200

    Fix javadoc

--- common/src/main/java/net/itskev/persistenceframework/common/api/MongoDbService.java
+++ common/src/main/java/net/itskev/persistenceframework/common/api/MongoDbService.java
@@ -5,11 +5,14 @@ import org.mongojack.JacksonMongoCollection;
 public interface MongoDbService {
 
   /**
-   * Returns a {@link JacksonMongoCollection<T>} for a given class.
+   * Returns a {@link JacksonMongoCollection} for a given class. This collection is a type-safe collection to work with
+   * POJO's directly.
    *
    * @param databaseName   The database where the collection is stored in
    * @param collectionName The name of the collection where the POJO's are stored
-   * @param type           The type of the POJO
+   * @param <T>            The type of the class
+   * @param type           The POJO's class
+   * @return a {@link JacksonMongoCollection} for the given class.
    */
   <T> JacksonMongoCollection<T> getCollection(String databaseName, String collectionName, Class<T> type);
 }
